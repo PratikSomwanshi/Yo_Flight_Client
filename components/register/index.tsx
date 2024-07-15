f1f678bbd2887ddd64e1ee0f59abd8e092cf7b6f
Pratik Somwanshi <pratiksomwanshi570@gmail.com>
2024-07-15 10:34:36 +0530

    add: responsiveness to register page and select country

--- components/register/index.tsx
+++ components/register/index.tsx
@@ -3,6 +3,8 @@ import { register } from "@/utils/actions"; // Ensure you implement this action
 import React, { FormEvent, useState } from "react";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
+import Select from "react-select";
+import { set } from "date-fns";
 
 function Register() {
     const [username, setUsername] = useState("");
@@ -17,7 +19,7 @@ function Register() {
     const [country, setCountry] = useState("");
     const [errorMessage, setErrorMessage] = useState("");
 
-    const handleInputChange = () => {
+    const handleInputChangeErase = () => {
         setErrorMessage("");
     };
 
@@ -45,6 +47,20 @@ function Register() {
             return;
         }
 
+        console.log({
+            username,
+            email,
+            fullName,
+            password,
+            address: {
+                line1,
+                city,
+                state,
+                postalCode,
+                country,
+            },
+        });
+
         let res;
         try {
             res = await register({
@@ -60,8 +76,6 @@ function Register() {
                     country,
                 },
             });
-
-            // Handle successful registration (e.g., redirect to login page)
         } catch (error: any) {
             if (error.message === "fetch failed") {
                 setErrorMessage("Something went wrong. Please try again.");
@@ -75,150 +89,152 @@ function Register() {
             return;
         }
     };
+    type Country = {
+        name: string;
+        code: string;
+    };
+
+    const countries: Country[] = [{ name: "India", code: "in" }];
+
+    const countryOptions = countries.map((country) => ({
+        value: country.code,
+        label: country.name,
+    }));
+
+    const [selectedCountry, setSelectedCountry] = useState<{
+        value: string;
+        label: string;
+    } | null>(null);
+
+    const handleChange = (
+        selectedOption: { value: string; label: string } | null
+    ) => {
+        setSelectedCountry(selectedOption);
+        setCountry(selectedOption?.value || "");
+    };
 
     return (
-        <div className="w-screen h-full mt-10 mb-10  flex justify-center items-center rounded-md">
-            <div className="bg-slate-100 w-[36%] py-10 h-[85%] px-3">
+        <div className="w-screen h-full mt-10 mb-10 flex justify-center items-center rounded-md">
+            <div className="bg-slate-100 w-full sm:w-[80%] md:w-[60%] lg:w-[36%] py-10 h-[85%] px-3">
                 <h1 className="mb-6 mt-4 underline text-lg text-center">
                     User Registration
                 </h1>
                 <form onSubmit={handleSubmit} className="px-2">
                     <div className="space-y-1">
                         <label htmlFor="username">Username:</label>
-                        <Input
+                        <input
                             type="text"
                             id="username"
-                            className="bg-gray-200"
+                            className="bg-gray-200 w-full p-2 rounded"
                             value={username}
-                            onChange={(e) => {
-                                setUsername(e.target.value);
-                                handleInputChange();
-                            }}
+                            onChange={(e) => setUsername(e.target.value)}
                         />
                     </div>
                     <div className="space-y-1 mt-2">
                         <label htmlFor="email">Email:</label>
-                        <Input
+                        <input
                             type="email"
                             id="email"
-                            className="bg-gray-200"
+                            className="bg-gray-200 w-full p-2 rounded"
                             value={email}
-                            onChange={(e) => {
-                                setEmail(e.target.value);
-                                handleInputChange();
-                            }}
+                            onChange={(e) => setEmail(e.target.value)}
                         />
                     </div>
                     <div className="space-y-1 mt-2">
                         <label htmlFor="fullName">Full Name:</label>
-                        <Input
+                        <input
                             type="text"
                             id="fullName"
-                            className="bg-gray-200"
+                            className="bg-gray-200 w-full p-2 rounded"
                             value={fullName}
-                            onChange={(e) => {
-                                setFullName(e.target.value);
-                                handleInputChange();
-                            }}
+                            onChange={(e) => setFullName(e.target.value)}
                         />
                     </div>
                     <div className="space-y-1 mt-2">
                         <label htmlFor="password">Password:</label>
-                        <Input
+                        <input
                             type="password"
                             id="password"
-                            className="bg-gray-200"
+                            className="bg-gray-200 w-full p-2 rounded"
                             value={password}
-                            onChange={(e) => {
-                                setPassword(e.target.value);
-                                handleInputChange();
-                            }}
+                            onChange={(e) => setPassword(e.target.value)}
                         />
                     </div>
                     <div className="space-y-1 mt-2">
                         <label htmlFor="confirmPassword">
                             Confirm Password:
                         </label>
-                        <Input
+                        <input
                             type="password"
                             id="confirmPassword"
-                            className="bg-gray-200"
+                            className="bg-gray-200 w-full p-2 rounded"
                             value={confirmPassword}
-                            onChange={(e) => {
-                                setConfirmPassword(e.target.value);
-                                handleInputChange();
-                            }}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
                         />
                     </div>
                     <div className="space-y-1 mt-2">
                         <label htmlFor="line1">Address Line 1:</label>
-                        <Input
+                        <input
                             type="text"
                             id="line1"
-                            className="bg-gray-200"
+                            className="bg-gray-200 w-full p-2 rounded"
                             value={line1}
-                            onChange={(e) => {
-                                setLine1(e.target.value);
-                                handleInputChange();
-                            }}
+                            onChange={(e) => setLine1(e.target.value)}
                         />
                     </div>
                     <div className="space-y-1 mt-2">
                         <label htmlFor="city">City:</label>
-                        <Input
+                        <input
                             type="text"
                             id="city"
-                            className="bg-gray-200"
+                            className="bg-gray-200 w-full p-2 rounded"
                             value={city}
-                            onChange={(e) => {
-                                setCity(e.target.value);
-                                handleInputChange();
-                            }}
+                            onChange={(e) => setCity(e.target.value)}
                         />
                     </div>
                     <div className="space-y-1 mt-2">
                         <label htmlFor="state">State:</label>
-                        <Input
+                        <input
                             type="text"
                             id="state"
-                            className="bg-gray-200"
+                            className="bg-gray-200 w-full p-2 rounded"
                             value={state}
-                            onChange={(e) => {
-                                setState(e.target.value);
-                                handleInputChange();
-                            }}
+                            onChange={(e) => setState(e.target.value)}
                         />
                     </div>
                     <div className="space-y-1 mt-2">
                         <label htmlFor="postalCode">Postal Code:</label>
-                        <Input
+                        <input
                             type="text"
                             id="postalCode"
-                            className="bg-gray-200"
+                            className="bg-gray-200 w-full p-2 rounded"
                             value={postalCode}
-                            onChange={(e) => {
-                                setPostalCode(e.target.value);
-                                handleInputChange();
-                            }}
+                            onChange={(e) => setPostalCode(e.target.value)}
                         />
                     </div>
                     <div className="space-y-1 mt-2">
                         <label htmlFor="country">Country:</label>
-                        <Input
-                            type="text"
+                        <Select
                             id="country"
-                            className="bg-gray-200"
-                            value={country}
-                            onChange={(e) => {
-                                setCountry(e.target.value);
-                                handleInputChange();
-                            }}
+                            options={countryOptions}
+                            value={selectedCountry}
+                            onChange={handleChange}
+                            className="bg-gray-200 w-full p-2 rounded"
+                            placeholder="Select Country"
                         />
+                        {selectedCountry && (
+                            <input
+                                type="hidden"
+                                value={selectedCountry.value}
+                            />
+                        )}
                     </div>
                     <div className="w-full flex justify-center items-center mt-6">
-                        <Button type="submit" className="w-[70%] bg-green-500">
+                        <button
+                            type="submit"
+                            className="w-full md:w-[70%] bg-green-500 p-2 rounded text-white hover:bg-green-600">
                             Register
-                        </Button>
+                        </button>
                     </div>
                     {errorMessage && (
                         <p className="text-red-400 mt-3 text-center">
